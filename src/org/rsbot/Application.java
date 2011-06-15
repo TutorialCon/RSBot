89a917da375efbbc6489b80f7d414052d797e484
Paris <paris@powerbot.org>
2011-06-15 21:51:16 +0100

    License acceptance window.

--- src/org/rsbot/Application.java
+++ src/org/rsbot/Application.java
@@ -2,20 +2,43 @@ package org.rsbot;
 
 import org.rsbot.bot.Bot;
 import org.rsbot.gui.BotGUI;
+import org.rsbot.gui.LicenseDialog;
 import org.rsbot.gui.LoadScreen;
+import org.rsbot.util.io.IOHelper;
 
 import java.awt.*;
+import java.io.File;
 
 public class Application {
 	private static BotGUI gui;
+	final static File licenseFile = new File(Configuration.Paths.getLicenseAcceptance());
 
 	public static void main(final String[] args) {
+		if (!isLicenseAccepted()) {
+			if (!LicenseDialog.showDialog(null)) {
+				return;
+			}
+		}
+		IOHelper.write(Integer.toString(Configuration.getVersion()), licenseFile);
 		final LoadScreen loader = new LoadScreen();
 		if (!loader.error) {
 			gui = new BotGUI();
 			loader.dispose();
 			gui.setVisible(true);
 		}
+	}
+
+	private static boolean isLicenseAccepted() {
+		if (!licenseFile.exists() || !licenseFile.canRead()) {
+			return false;
+		}
+		final int version;
+		try {
+			version = Integer.parseInt(IOHelper.readString(licenseFile));
+		} catch (final Exception ignored) {
+			return false;
+		}
+		return Configuration.getVersion() == version;
 	}
 
 	public static Bot getBot(final Object o) {
