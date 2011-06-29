d743159857a1eddef6f847f3b73ec7a73eb1eede
Joe <Timer150@Gmail.com>
2011-06-29 18:01:53 -0400

    Revert "Fully removed bot home tab."
    This reverts commit 46f9ae6d8cf58fa5fede20a74dc9e3eda26f9dcd.

---
+++ src/org/rsbot/gui/BotHome.java
@@ -0,0 +1,18 @@
+package org.rsbot.gui;
+
+import java.awt.*;
+
+/**
+ * @author Timer
+ */
+class BotHome {
+	private int width, height;
+
+	public void setSize(final int width, final int height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	public void paint(final Graphics g) {
+	}
+}
