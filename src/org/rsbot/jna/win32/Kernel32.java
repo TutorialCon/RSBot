493445887aa69488d0dd971d416188f3ec03e229
Paris <paris@powerbot.org>
2011-07-02 11:39:30 +0100

    Avoid static init.

--- src/org/rsbot/jna/win32/Kernel32.java
+++ src/org/rsbot/jna/win32/Kernel32.java
@@ -1,14 +1,11 @@
 package org.rsbot.jna.win32;
 
-import com.sun.jna.Native;
 import com.sun.jna.win32.StdCallLibrary;
 
 /**
  * @author Paris
  */
 public interface Kernel32 extends StdCallLibrary {
-	Kernel32 INSTANCE = (Kernel32) Native.loadLibrary("kernel32", Kernel32.class);
-
 	final int PROCESS_SET_QUOTA = 0x0100;
 	final int PROCESS_SET_INFORMATION = 0x0200;
 	final int PROCESS_QUERY_INFORMATION = 0x0400;
