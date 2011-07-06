5139e906e31edeb318c66afc8321d443c73fa077
Joe <Timer150@Gmail.com>
2011-07-05 21:32:01 -0400

    Updated background scripts.

--- src/org/rsbot/script/BackgroundScript.java
+++ src/org/rsbot/script/BackgroundScript.java
@@ -1,5 +1,7 @@
 package org.rsbot.script;
 
+import java.util.logging.Level;
+
 /**
  * A background script.
  *
@@ -8,37 +10,38 @@ package org.rsbot.script;
 public abstract class BackgroundScript extends Script {
 	protected abstract boolean activateCondition();
 
-	protected abstract int iterationSleep();
-
 	@Override
-	public void stopScript(final boolean logout) {
-		running = false;
-	}
-
-	public final void run() {
-		ctx.bot.getEventManager().addListener(this);
-		running = true;
+	public void run() {
+		setRunning(true);
 		try {
-			while (running) {
+			onRun();
+		} catch (final ThreadDeath ignored) {
+		} catch (final Throwable ex) {
+			log.log(Level.SEVERE, "Error starting task: ", ex);
+		}
+		try {
+			while (isRunning()) {
 				if (activateCondition()) {
-					final boolean start = onStart();
-					if (start) {
-						while (running) {
-							final int timeOut = loop();
-							if (timeOut == -1) {
-								break;
-							}
-							Thread.sleep(timeOut);
-						}
-						onFinish();
+					if (!runOnce()) {
+						break;
+					}
+				} else {
+					try {
+						sleep(pausedIterationDelay());
+					} catch (final ThreadDeath td) {
+						break;
 					}
 				}
-				Thread.sleep(iterationSleep());
 			}
-		} catch (final Exception e) {
-			e.printStackTrace();
+			try {
+				onFinish();
+			} catch (final ThreadDeath ignored) {
+			} catch (final RuntimeException e) {
+				e.printStackTrace();
+			}
+		} catch (final Throwable t) {
+			onFinish();
 		}
-		ctx.bot.getEventManager().removeListener(this);
-		running = false;
+		super.stop();
 	}
 }
