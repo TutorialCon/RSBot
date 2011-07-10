61fb61c8f014449e4229bbe709ad273781746982
Joe <Timer150@Gmail.com>
2011-07-09 22:12:07 -0400

    Made it static since the threadgroup is static.

--- src/org/rsbot/script/task/executor/ScriptPool.java
+++ src/org/rsbot/script/task/executor/ScriptPool.java
@@ -6,7 +6,7 @@ import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ScriptPool implements ThreadFactory {
-	private final AtomicInteger threadNumber = new AtomicInteger(1);
+	private static final AtomicInteger threadNumber = new AtomicInteger(1);
 
 	public Thread newThread(Runnable r) {
 		Thread thread = new Thread(ScriptHandler.THREAD_GROUP, r, ScriptHandler.THREAD_GROUP_NAME + "-" + threadNumber.getAndIncrement());
