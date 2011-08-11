4f901da7c082004e39aed55f2bbe1d94749801f2
Dunnkers Habinio <dunnkers@hotmail.com>
2011-08-11 17:26:32 +0200

    Fixed a bug where if you'd mark multiple categories in the script selector, it'd show no scripts.

--- src/org/rsbot/gui/ScriptSelector.java
+++ src/org/rsbot/gui/ScriptSelector.java
@@ -485,27 +485,26 @@ public class ScriptSelector extends JDialog implements ScriptListener {
 
 		public void search(final String find, final String[] keys, final boolean likedOnly) {
 			matches.clear();
-			for (final ScriptDefinition def : scripts) {
+			outer: for (final ScriptDefinition def : scripts) {
 				if (likedOnly && !ScriptLikes.isLiked(def)) {
 					continue;
 				}
 				if (find.length() != 0 && !def.name.toLowerCase().contains(find)) {
 					continue;
 				}
+				if (!(keys.length > 0)) {
+					matches.add(def);
+				}
 				final List<String> keywords = def.getKeywords();
 				final ArrayList<String> list = new ArrayList<String>(keywords.size());
 				for (final String key : keywords) {
 					list.add(key.toLowerCase());
 				}
-				boolean hit = true;
 				for (final String key : keys) {
-					if (!list.contains(key)) {
-						hit = false;
-						break;
+					if (list.contains(key)) {
+						matches.add(def);
+						continue outer;
 					}
-				}
-				if (hit) {
-					matches.add(def);
 				}
 			}
 			fireTableDataChanged();
