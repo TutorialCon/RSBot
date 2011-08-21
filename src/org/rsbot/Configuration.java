67b99f99cd6ca293970980aad291f88f1032943f
Wang Can <paris@powerbot.org>
2011-08-21 15:39:23 +0100

    Removed unused variable.

--- src/org/rsbot/Configuration.java
+++ src/org/rsbot/Configuration.java
@@ -206,7 +206,6 @@ public class Configuration {
 	public static final String NAME_LOWERCASE = NAME.toLowerCase();
 	private static final OperatingSystem CURRENT_OS;
 	public static boolean RUNNING_FROM_JAR = false;
-	public static final String SKIN = "org.pushingpixels.substance.api.skin.SubstanceGraphiteGlassLookAndFeel";
 
 	public static class Twitter {
 		public static final boolean ENABLED = true;
